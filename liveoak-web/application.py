64d9196315e0f18c0301684d723dac4234fe49bc
Rohan Rao <rsrao@ncsu.edu>
2023-02-27 00:16:47 -0500

    Implement OPA with sample policy and decisions

--- liveoak-web/application.py
+++ liveoak-web/application.py
@@ -2,9 +2,11 @@
 """
 
 import json
+import time
+import http.client
 from os import environ as env
 from urllib.parse import quote_plus, urlencode
-
+from opa_client.opa import OpaClient
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, redirect, render_template, session, url_for
@@ -15,7 +17,6 @@ if ENV_FILE:
 
 application = Flask(__name__)
 application.secret_key = env.get("APP_SECRET_KEY")
-
 
 oauth = OAuth(application)
 
@@ -69,6 +70,95 @@ def logout():
             quote_via=quote_plus,
         )
     )
+
+
+@application.route("/roles")
+def roles():
+    try:
+        if session.get("user") is None:
+            return redirect("/login")
+
+        user_id = session.get("user")["userinfo"]["sub"]
+
+        conn = http.client.HTTPSConnection(env.get("AUTH0_DOMAIN"))
+        payload = json.dumps({
+            "client_id": env.get("AUTH0_CLIENT_ID"),
+            "client_secret": env.get("AUTH0_CLIENT_SECRET"),
+            "audience": "https://" + env.get("AUTH0_DOMAIN") + "/api/v2/",
+            "grant_type": "client_credentials",
+        })
+        headers = {'content-type': "application/json"}
+        conn.request("POST", "/oauth/token", payload, headers)
+
+        token_res = json.loads(
+            conn.getresponse().read().decode("utf-8"))
+
+        client_token = token_res["access_token"]
+
+        headers = {'Authorization': "Bearer " + client_token}
+        conn.request("GET", f"/api/v2/users/{user_id}/roles", headers=headers)
+
+        roles = conn.getresponse().read().decode("utf-8")
+
+        print("User roles: " + roles)
+
+        roles = [{"name": role["name"], "description": role["description"]}
+                 for role in json.loads(roles)]
+
+        return json.dumps(roles)
+    except Exception as e:
+        return "An error occurred when retrieving roles: " + str(e)
+
+
+@application.route("/opa")
+def opa():
+
+    try:
+        response = ''
+
+
+        client = OpaClient()  # default host='localhost', port=8181, version='v1'
+
+        client.update_opa_policy_fromfile("policy.rego", "testpolicy")
+
+        inputs = [
+            {
+                "input": {
+                    "method": "GET",
+                    "roles": ["owner"],
+                    "params": {
+                        "amount": 500
+                    }
+                }
+            },
+            {
+                "input": {
+                    "method": "POST",
+                    "roles": ["owner"],
+                    "params": {
+                        "amount": 501
+                    }
+                }
+            },
+            {
+                "input": {
+                    "method": "POST",
+                    "roles": ["owner"],
+                    "params": {
+                        "amount": 500
+                    }
+                }
+            }
+        ]
+
+        for i in inputs:
+            response += json.dumps(i) + " ==> " + str(client.check_permission(
+                input_data=i, policy_name="testpolicy", rule_name="transfers")) + '<br/>'
+
+        return response
+
+    except Exception as e:
+        return str(e)
 
 
 if __name__ == "__main__":
